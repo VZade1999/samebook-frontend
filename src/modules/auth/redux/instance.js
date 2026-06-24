cffe54bc4becd1bdbf3894c657eb025c1719e2b7
VZade1999 <vzade1999@gmail.com>
2026-06-25 00:47:39 +0530

    fix(fox alogin url)

--- src/modules/auth/redux/instance.js
+++ src/modules/auth/redux/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_AUTHN_API_URL || "http://localhost:3020",
+  baseURL: "https://samebook-authn-backend.vercel.app/",
   withCredentials: true,
 });
 
