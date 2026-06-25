eeed126ff1e48af633bcca76b0f1dc7a7bf353b2
VZade1999 <vzade1999@gmail.com>
2026-06-25 21:27:52 +0530

    change instance file tyep

--- src/modules/auth/redux/index.ts
+++ src/modules/auth/redux/index.ts
@@ -2,7 +2,7 @@ import logMessage from "../../../utils/logger";
 import instance from "./instance";
 
 class AuthnService {
-  login = async (payload) => {
+  login = async (payload:any) => {
     try {
       return await instance.post(`/auth/login`, payload);
     } catch (error) {
@@ -11,7 +11,7 @@ class AuthnService {
     }
   };
 
-  forgotPassword = async (payload) => {
+  forgotPassword = async (payload:any) => {
     try {
       return await instance.post(`/auth/forgot-password`, payload);
     } catch (error) {
@@ -20,7 +20,7 @@ class AuthnService {
     }
   };
 
-  confirmForgotPassword = async (payload) => {
+  confirmForgotPassword = async (payload:any) => {
     try {
       return await instance.post(`/auth/confirm-forgot-password`, payload);
     } catch (error) {
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,4 @@
-const logMessage = (location, message) => {
+const logMessage = (location:any, message:any) => {
   const env = process.env.NODE_ENV;
   if (env === "local" || env === "uat") {
     console.log(location, +"-->" + message);
