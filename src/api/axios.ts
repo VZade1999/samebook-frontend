ae41bb0198300a74d347d26b03a5911a3d1e842f
VZade1999 <vzade1999@gmail.com>
2026-08-10 20:32:15 +0530

    fix base api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.REACT_APP_CUSTOMERS_API_URL ||
-  "http://localhost:3010";
+  "https://samebook-customer-backend-dev.vercel.app";
 
 const apiClient = axios.create({
   baseURL,
